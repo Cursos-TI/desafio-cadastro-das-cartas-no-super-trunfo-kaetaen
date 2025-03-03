f604e5f6675e3afd4f265995c78fbeb106e4423e
kaetaen <kaetaen@tutanota.com>
2025-03-03 14:32:06 -0300

    fix: ajustes de tipo, entrada e saida para  área e PIB.

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -2,23 +2,23 @@
 // Tema 1 - Cadastro das Cartas
 // Este código inicial serve como base para o desenvolvimento do sistema de cadastro de cartas de cidades.
 // Siga os comentários para implementar cada parte do desafio.
-// Aluno: José Rubens Dos Santos Oliveira 
+// Aluno: José Rubens Dos Santos Oliveira
 
 #include <stdio.h>
 
 int main () {
 
     // DECLARAÇÃO VARIAVEL CARTA 01
-    char PIB__01[200];
-    char area__01[200];
+    float PIB__01;
+    float area__01;
     char estado__01[2];
     char codigo__01[4];
     char nome_da_cidade__01[200];
     int populacao__01, numero_pontos_turisticos__01;
 
     // DECLARAÇÃO VARIAVEL CARTA 02
-    char PIB__02[200];
-    char area__02[200];
+    float PIB__02;
+    float area__02;
     char estado__02[2];
     char codigo__02[4];
     char nome_da_cidade__02[200];
@@ -41,10 +41,10 @@ int main () {
     scanf("%i", &populacao__01);
 
     printf("Insira a área: ");
-    scanf("%s", area__01);
+    scanf("%f", &area__01);
 
     printf("Insira o PIB: ");
-    scanf("%s", PIB__01);
+    scanf("%f", &PIB__01);
 
     printf("Insira o numero de pontos turisticos: ");
     scanf("%i", &numero_pontos_turisticos__01);
@@ -66,10 +66,10 @@ int main () {
     scanf("%i", &populacao__02);
 
     printf("Insira a área: ");
-    scanf("%s", area__02);
+    scanf("%f", &area__02);
 
     printf("Insira o PIB: ");
-    scanf("%s", PIB__02);
+    scanf("%f", &PIB__02);
 
     printf("Insira o numero de pontos turisticos: ");
     scanf("%i", &numero_pontos_turisticos__02);
@@ -82,8 +82,8 @@ int main () {
     printf("Código: %s\n", codigo__01);
     printf("Nome da cidade: %s\n", nome_da_cidade__01);
     printf("População: %i\n", populacao__01);
-    printf("Área: %s\n", area__01);
-    printf("PIB: %s\n", PIB__01);
+    printf("Área: %.2f km²\n", area__01);
+    printf("PIB: %.2f bilhões de reais \n", PIB__01);
     printf("Número de pontos túristicos: %i\n\n", numero_pontos_turisticos__01);
     printf("-------------------------------------");
 
@@ -93,8 +93,8 @@ int main () {
     printf("Código: %s\n", codigo__02);
     printf("Nome da cidade: %s\n", nome_da_cidade__02);
     printf("População: %i\n", populacao__02);
-    printf("Área: %s\n", area__02);
-    printf("PIB: %s\n", PIB__02);
+    printf("Área: %.2f km²\n", area__02);
+    printf("PIB: %.2f bilhões de reais \n", PIB__02);
     printf("Número de pontos túristicos: %i\n", numero_pontos_turisticos__02);
     
 }
