fcf44a97054d0e2846a9b10fb37960b521b5c474
kaetaen <kaetaen@tutanota.com>
2025-03-04 15:28:16 -0300

    feat: comparação de atributo população via operadores relacionais

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -15,7 +15,7 @@ int main () {
     // DECLARAÇÃO VARIAVEL CARTA 01
     float PIB__01;
     float area__01;
-    char estado__01[2];
+    char estado__01[3];
     char codigo__01[4];
     char nome_da_cidade__01[200];
     unsigned long int populacao__01;
@@ -27,7 +27,7 @@ int main () {
     // DECLARAÇÃO VARIAVEL CARTA 02
     float PIB__02;
     float area__02;
-    char estado__02[2];
+    char estado__02[3];
     char codigo__02[4];
     char nome_da_cidade__02[200];
     unsigned long int populacao__02;
@@ -100,48 +100,14 @@ int main () {
     super_poder__02 = (float)numero_pontos_turisticos__02 + populacao__02 + PIB__02 + area__02 + pibPerCapta__02 + (1 / densidadePopulacional__01);
 
     printf("-------------------------------------\n");
-    printf("Comparação de Cartas: \n");
-    printf("-------------------------------------\n");
 
+    printf("Carta 1 - %s (%s): %li\n", nome_da_cidade__01, estado__01, populacao__01);
+    printf("Carta 1 - %s (%s): %li\n", nome_da_cidade__02, estado__02, populacao__02);
     if (populacao__01 > populacao__02){
-        printf("População: Carta 1 venceu \n");
+        printf("Resultado: Carta 1 (%s) venceu!\n", nome_da_cidade__01);
     } else {
-        printf("População: Carta 2 venceu \n");
+        printf("Resultado: Carta 2 (%s) venceu!\n", nome_da_cidade__02);
     }
     
-    if (area__01 > area__02){
-        printf("Área: Carta 1 venceu\n");
-    } else {
-        printf("Área: Carta 2 venceu\n");
-    }
-    
-    if (PIB__01 > PIB__02){
-        printf("PIB: Carta 1 venceu \n");
-    } else {
-        printf("PIB: Carta 2 venceu \n");
-    }
-    
-    if (numero_pontos_turisticos__01 > numero_pontos_turisticos__02){
-        printf("Pontos Turisticos: Carta 1 venceu \n");
-    } else {
-        printf("Pontos Turisticos: Carta 2 venceu \n");
-    }
-
-    if (densidadePopulacional__01 < densidadePopulacional__02){
-        printf("Densidade Populacional: Carta 1 venceu \n");
-    } else {
-        printf("Densidade Populacional: Carta 2 venceu \n");
-    }
-
-    if (pibPerCapta__01 > pibPerCapta__02){
-        printf("PIB Per Capta: Carta 1 venceu \n");
-    } else {
-        printf("PIB Per Capta: Carta 2 venceu \n");
-    }
-
-    if (super_poder__01 > super_poder__02){
-        printf("Superpoder: Carta 1 venceu \n");
-    } else {
-        printf("Superpoder: Carta 2 venceu \n");
-    }   
+       
 }
